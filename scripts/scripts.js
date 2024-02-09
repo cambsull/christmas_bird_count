1be661791f8a3f7525ea9054c5991cbe9c67b3ee
Cambria Sullivan <72573258+cambsull@users.noreply.github.com>
2024-02-08 18:58:13 -0800

    Added hamburger menu, scroll functions, menu slideout functions.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,23 @@
-const testLog = () => {
-    console.log("Test Log Executed");
-    return null;
+//OnLoad functions
+window.onload = () => {
+    //Handle scroll events
+    window.addEventListener('scroll', function (e) {
+        window.scrollY > 100 ?
+            this.document.querySelector("header").classList.add('is-scrolling') :
+            this.document.querySelector("header").classList.remove('is-scrolling');
+    })
+
+    //Hamburger Menu
+    const menu_btn = document.querySelector('.hamburger');
+    const mobile_menu = document.querySelector('.mobile-nav')
+
+    menu_btn.addEventListener('click', function () {
+        menu_btn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
+    })
+    
 }
 
-testLog();
+
+
+
